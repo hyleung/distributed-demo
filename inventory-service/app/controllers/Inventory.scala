6561bcabb6a03e29b8929a539d7768a2d78c484f
Ho Yan Leung <hy.leung@gmail.com>
2015-04-14 05:39:38 -0700

    Implements fake inventory service

---
+++ inventory-service/app/controllers/Inventory.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import play.api.libs.json.Writes
+import play.api.mvc.{Action, Controller}
+import play.api.libs.json._
+
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import scala.util.Random
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 15-04-13
+ * Time: 6:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object Inventory extends Controller{
+
+  def retrieveStoreInventory(id:String) = Action {
+    val json = Json.toJson(
+      List(
+        StoreAvailability("Cambie",100),
+        StoreAvailability("Downtown",100),
+        StoreAvailability("Broadway",100),
+        StoreAvailability("Burnaby",100)
+      )
+    )
+    Ok(json)
+  }
+  implicit val resultWrite = new Writes[StoreAvailability] {
+    override def writes(o: StoreAvailability) = Json.obj(
+      "storeName" -> o.storeName,
+      "count" -> o.count
+    )
+
+  }
+}
+
+
+
+case class StoreAvailability(storeName:String, count:Int)
