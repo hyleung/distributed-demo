cc424d552270f3db0561352bf7b250e33173355b
Ho Yan Leung <hy.leung@gmail.com>
2015-04-16 21:26:52 -0700

    Formatting cleanup

--- inventory-service/app/controllers/Application.scala
+++ inventory-service/app/controllers/Application.scala
@@ -1,37 +1,40 @@
 package controllers
 
+import play.api.Play.current
 import play.api._
 import play.api.libs.functional.syntax._
 import play.api.libs.json.{JsPath, Reads}
 import play.api.mvc._
-import play.api.Play.current
+
 import scala.language.postfixOps
+
 object Application extends Controller {
-  var errorRate = Play.application.configuration.getDouble("service.errorRate").get
-  var minDelay  = Play.application.configuration.getInt("service.minDelay").get
-  var maxDelay = Play.application.configuration.getInt("service.maxDelay").get
-  def index = Action { implicit request =>
-    Ok(views.html.index(request, errorRate, minDelay, maxDelay))
-  }
+	var errorRate = Play.application.configuration.getDouble("service.errorRate").get
+	var minDelay = Play.application.configuration.getInt("service.minDelay").get
+	var maxDelay = Play.application.configuration.getInt("service.maxDelay").get
 
-  def submit = Action(parse.json) { implicit request =>
-    request.body.asOpt[ServiceSettings] match {
-      case Some(settings) =>
-        errorRate = settings.errorRate
-        minDelay = settings.minDelay
-        maxDelay = settings.maxDelay
-        Ok(s"settings: $settings")
-      case None =>
-        BadRequest(request.body)
-    }
-  }
+	def index = Action { implicit request =>
+		Ok(views.html.index(request, errorRate, minDelay, maxDelay))
+	}
 
-  implicit val serviceSettingsRead:Reads[ServiceSettings] = (
-    (JsPath \ "errorRate").read[Double] and
-    (JsPath \ "minDelay").read[Int] and
-    (JsPath \ "maxDelay").read[Int]
-    )(ServiceSettings.apply _)
+	def submit = Action(parse.json) { implicit request =>
+		request.body.asOpt[ServiceSettings] match {
+			case Some(settings) =>
+				errorRate = settings.errorRate
+				minDelay = settings.minDelay
+				maxDelay = settings.maxDelay
+				Ok(s"settings: $settings")
+			case None =>
+				BadRequest(request.body)
+		}
+	}
+
+	implicit val serviceSettingsRead: Reads[ServiceSettings] = (
+		(JsPath \ "errorRate").read[Double] and
+			(JsPath \ "minDelay").read[Int] and
+			(JsPath \ "maxDelay").read[Int]
+		)(ServiceSettings.apply _)
 
 }
 
-case class ServiceSettings(errorRate:Double, minDelay:Int, maxDelay:Int)
+case class ServiceSettings(errorRate: Double, minDelay: Int, maxDelay: Int)
--- inventory-service/app/controllers/Inventory.scala
+++ inventory-service/app/controllers/Inventory.scala
@@ -1,16 +1,11 @@
 package controllers
 
-import java.lang.Exception
-
-import play.api.libs.json.Writes
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json.{Writes, _}
 import play.api.mvc.{Action, Controller}
-import play.api.libs.json._
 
 import scala.concurrent.Future
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
-
-import scala.util.parsing.json.JSONArray
-import scala.util.{Failure, Success, Try, Random}
+import scala.util.{Failure, Random, Success, Try}
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,44 +13,45 @@ import scala.util.{Failure, Success, Try, Random}
  * Time: 6:18 PM
  * To change this template use File | Settings | File Templates.
  */
-object Inventory extends Controller{
+object Inventory extends Controller {
 
-  def retrieveStoreInventory(id:String) = Action.async{
-    val timeout = Application.minDelay + Random.nextInt(Application.maxDelay)
+	def retrieveStoreInventory(id: String) = Action.async {
+		val timeout = Application.minDelay + Random.nextInt(Application.maxDelay)
 
-    val result = for {
-      x <- delay(timeout)
-      f <- fetchInventory(id)
-    } yield f
+		val result = for {
+			x <- delay(timeout)
+			f <- fetchInventory(id)
+		} yield f
 
-    result map {
-      case Success(availability) => Ok(Json.toJson(availability))
-      case Failure(err) => BadRequest("ruh-roh")
-    }
-  }
+		result map {
+			case Success(availability) => Ok(Json.toJson(availability))
+			case Failure(err) => BadRequest("ruh-roh")
+		}
+	}
 
-  private def fetchInventory(id:String):Future[Try[Seq[StoreAvailability]]] = Future {
-    Random.nextFloat() > Application.errorRate match {
-      case true => Success(     Seq(
-        StoreAvailability("Cambie", 100),
-        StoreAvailability("Downtown", 100),
-        StoreAvailability("Broadway", 100),
-        StoreAvailability("Burnaby", 100)
-      ))
-      case _ => Failure(new Exception())
-    }
-  }
+	private def fetchInventory(id: String): Future[Try[Seq[StoreAvailability]]] = Future {
+		Random.nextFloat() > Application.errorRate match {
+			case true => Success(Seq(
+				StoreAvailability("Cambie", 100),
+				StoreAvailability("Downtown", 100),
+				StoreAvailability("Broadway", 100),
+				StoreAvailability("Burnaby", 100)
+			))
+			case _ => Failure(new Exception())
+		}
+	}
 
-  private def delay(timeout:Int) = Future {
-    Thread.sleep(timeout)
-  }
-  implicit val resultWrite = new Writes[StoreAvailability] {
-    override def writes(o: StoreAvailability) = Json.obj(
-      "storeName" -> o.storeName,
-      "count" -> o.count
-    )
-  }
+	private def delay(timeout: Int) = Future {
+		Thread.sleep(timeout)
+	}
+
+	implicit val resultWrite = new Writes[StoreAvailability] {
+		override def writes(o: StoreAvailability) = Json.obj(
+			"storeName" -> o.storeName,
+			"count" -> o.count
+		)
+	}
 
 }
 
-case class StoreAvailability(storeName:String, count:Int)
+case class StoreAvailability(storeName: String, count: Int)
