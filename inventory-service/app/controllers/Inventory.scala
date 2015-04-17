26a0e71998a112a1eb22598ac7a568c893ffc999
Ho Yan Leung <hy.leung@gmail.com>
2015-04-16 20:23:17 -0700

    Refactor

--- inventory-service/app/controllers/Inventory.scala
+++ inventory-service/app/controllers/Inventory.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import java.lang.Exception
+
 import play.api.libs.json.Writes
 import play.api.mvc.{Action, Controller}
 import play.api.libs.json._
@@ -7,7 +9,8 @@ import play.api.libs.json._
 import scala.concurrent.Future
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-import scala.util.Random
+import scala.util.parsing.json.JSONArray
+import scala.util.{Failure, Success, Try, Random}
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,37 +20,42 @@ import scala.util.Random
  */
 object Inventory extends Controller{
 
-  def retrieveStoreInventory(id:String) = Action.async {
+  def retrieveStoreInventory(id:String) = Action.async{
     val timeout = Application.minDelay + Random.nextInt(Application.maxDelay)
 
-    val delay:Future[Boolean] = Future{
-      Thread.sleep(timeout)
-      !(Random.nextFloat() <= Application.errorRate)
-    }
-     delay.map {
-       case true =>
-         val json = Json.toJson(
-           List(
-             StoreAvailability("Cambie", 100),
-             StoreAvailability("Downtown", 100),
-             StoreAvailability("Broadway", 100),
-             StoreAvailability("Burnaby", 100)
-           )
-         )
-         Ok(json)
-       case _ => InternalServerError
-     }
+    val result = for {
+      x <- delay(timeout)
+      f <- fetchInventory(id)
+    } yield f
 
+    result map {
+      case Success(availability) => Ok(Json.toJson(availability))
+      case Failure(err) => BadRequest("ruh-roh")
+    }
+  }
+
+  private def fetchInventory(id:String):Future[Try[Seq[StoreAvailability]]] = Future {
+    Random.nextFloat() > Application.errorRate match {
+      case true => Success(     Seq(
+        StoreAvailability("Cambie", 100),
+        StoreAvailability("Downtown", 100),
+        StoreAvailability("Broadway", 100),
+        StoreAvailability("Burnaby", 100)
+      ))
+      case _ => Failure(new Exception())
+    }
+  }
+
+  private def delay(timeout:Int) = Future {
+    Thread.sleep(timeout)
   }
   implicit val resultWrite = new Writes[StoreAvailability] {
     override def writes(o: StoreAvailability) = Json.obj(
       "storeName" -> o.storeName,
       "count" -> o.count
     )
-
   }
+
 }
-
-
 
 case class StoreAvailability(storeName:String, count:Int)
