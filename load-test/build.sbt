d3174d1ef1b5fa3dec673e887fc9aabf0c627640
Ho Yan Leung <hy.leung@gmail.com>
2015-04-18 19:11:18 -0700

    Adds load test project

---
+++ load-test/build.sbt
@@ -0,0 +1,11 @@
+name := "load-test"
+
+version := "1.0"
+
+enablePlugins(GatlingPlugin)
+
+scalaVersion := "2.11.6"
+
+libraryDependencies ++= "io.gatling.highcharts" % "gatling-charts-highcharts" % "2.1.5" % "test"
+libraryDependencies ++= "io.gatling"            % "gatling-test-framework"    % "2.1.5" % "test"
+    
---
+++ load-test/src/test/scala/InventoryServiceSimulation.scala
@@ -0,0 +1,22 @@
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+import scala.language.postfixOps
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 15-04-18
+ * Time: 11:42 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class InventoryServiceSimulation extends Simulation{
+
+	val httpConf = http
+		.baseURL("http://localhost:9000")
+
+	val scn = scenario("Scenario Name")
+		.exec(http("inventory")
+			.get("/inventory/1234"))
+			.pause(1)
+
+	setUp(scn.inject(rampUsers(100) over (10 seconds)).protocols(httpConf))
+}
