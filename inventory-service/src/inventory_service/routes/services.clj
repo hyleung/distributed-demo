4f976513bf04c58689037feb6f6aadf916f27790
Ho Yan Leung <hy.leung@gmail.com>
2015-04-22 21:17:21 -0700

    Initial commit Clojure web service
    Stubbed out inventory service using Luminus framework.

---
+++ inventory-service/src/inventory_service/routes/services.clj
@@ -0,0 +1,26 @@
+(ns inventory-service.routes.services
+  (:require [ring.util.http-response :refer :all]
+            [compojure.api.sweet :refer :all]
+            [schema.core :as s]))
+
+(s/defschema AvailabilityInfo {:id s/Str 
+                               :inStock s/Bool
+                               :count  s/Int })
+
+(defapi service-routes
+  (ring.swagger.ui/swagger-ui
+    "/swagger")
+  (swagger-docs
+    :title "ABC Company eCommerce")
+  (swaggered "Inventory"
+             :description "Warehouse Inventory Service"
+             (context "/api" []
+
+                      (GET* "/availability/:productId" [productId]
+                            :return      AvailabilityInfo 
+                            :query-params []
+                            :summary      "Retrieve availability information for a given product"
+                            (ok {:id productId :inStock true :count 100}))
+                      )
+             )
+  )
