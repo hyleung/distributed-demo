044974c654efca266f48d8341e68cf171379cc6b
Ho Yan Leung <hy.leung@gmail.com>
2015-05-02 08:23:07 -0700

    Adds settings

--- inventory-service/src/inventoryService/logic/inventory.clj
+++ inventory-service/src/inventoryService/logic/inventory.clj
@@ -1,5 +1,9 @@
 (ns inventoryService.logic.inventory)
 
+(defrecord ServiceSettings [errorRate minLatency maxLatency])
+
+(def service-settings (atom (ServiceSettings. 0 0 0)))
+
 ;;roll the dice for an error
 (defn roll-dice[errorRate]
   (< rand errorRate))
@@ -10,3 +14,10 @@
 (defn fetchInventory[productId]
   {:id productId :inStock true :count (fetchCount )})
 
+(defn setSettings[errorRate minLatency maxLatency]
+  (do
+    (swap! service-settings (fn[x] (ServiceSettings. errorRate minLatency maxLatency))) @service-settings
+  ))
+
+(defn getSettings[]
+  @service-settings)
--- inventory-service/test/inventoryService/test/inventory.clj
+++ inventory-service/test/inventoryService/test/inventory.clj
@@ -12,11 +12,26 @@
       (is (= expected (result :id))))))
 
 (deftest test-fetch-inventory
-  (let [expectedCount 200]
-    (with-redefs [inventoryService.logic.inventory/fetchCount (fn[] expectedCount)]
+  (let [expectedCount 300]
+    (with-redefs [fetchCount (fn[] expectedCount)]
       (testing "should fetch count"
         (let [ expected "abcde"
                result (fetchInventory expected)]
           (is (= expectedCount (result :count)))
           ))))
   )
+
+(deftest test-settings
+  (testing "should initialize settings to defaults"
+    (let [result (getSettings)]
+      (is (= 0 (:errorRate result)))
+      (is (= 0 (:minLatency result)))
+      (is (= 0 (:maxLatency result)))
+      ))
+  (testing "should set settings"
+    (setSettings 0.05 500 500)
+    (let [setting (getSettings)]
+      (is (= 0.05 (:errorRate setting)))
+      (is (= 500 (:minLatency setting)))
+      (is (= 500 (:maxLatency setting)))
+      )))
