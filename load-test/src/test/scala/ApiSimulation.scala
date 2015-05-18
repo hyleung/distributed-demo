7539b760c0da55ae8d5103c5c647893a039a3444
Ho Yan Leung <hy.leung@gmail.com>
2015-05-17 19:11:10 -0700

    Adds repeat on detail calls

--- load-test/src/test/scala/ApiSimulation.scala
+++ load-test/src/test/scala/ApiSimulation.scala
@@ -2,6 +2,8 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 import scala.language.postfixOps
+import scala.util.Random
+
 /**
  * Created with IntelliJ IDEA.
  * Date: 15-04-18
@@ -18,9 +20,15 @@ class ApiSimulation extends Simulation{
 			.get("/api/catalog"))
 			.pause(1)
 		.randomSwitch(
-			75.0 -> exec(http("Product detail").get("/api/product/50")),
-			25.0 -> exec(http("Product detail w/availability").get("/api/product/50?storeAvailability=true"))
-		).pause(1)
+			75.0 -> repeat(5) {
+				exec(http("Product detail")
+					.get("/api/product/50"))
+					.pause(1)},
+			25.0 -> repeat(5) {
+				exec(http("Product detail w/availability")
+					.get("/api/product/50?storeAvailability=true"))
+					.pause(1)}
+		)
 
 	setUp(scn.inject(rampUsers(250) over (60 seconds)).protocols(httpConf))
 }
