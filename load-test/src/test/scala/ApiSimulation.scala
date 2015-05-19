50b1b21a68316362f106d0391bb814e7ef82dc5a
Ho Yan Leung <hy.leung@gmail.com>
2015-05-18 20:29:57 -0700

    Tweak load test params

--- load-test/src/test/scala/ApiSimulation.scala
+++ load-test/src/test/scala/ApiSimulation.scala
@@ -2,7 +2,6 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 import scala.language.postfixOps
-import scala.util.Random
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,25 +9,29 @@ import scala.util.Random
  * Time: 11:42 AM
  * To change this template use File | Settings | File Templates.
  */
-class ApiSimulation extends Simulation{
+class ApiSimulation extends Simulation {
 
 	val httpConf = http
 		.baseURL("http://localhost:8080")
 
 	val scn = scenario("Browse")
 		.exec(http("Fetch product list")
-			.get("/api/catalog"))
-			.pause(1)
+		.get("/api/catalog"))
+		.pause(1)
 		.randomSwitch(
-			75.0 -> repeat(5) {
+			75.0 -> repeat(3) {
 				exec(http("Product detail")
 					.get("/api/product/50"))
-					.pause(1)},
-			25.0 -> repeat(5) {
+					.pause(1)
+			},
+			25.0 -> repeat(3) {
 				exec(http("Product detail w/availability")
 					.get("/api/product/50?storeAvailability=true"))
-					.pause(1)}
+					.pause(1)
+			}
 		)
 
-	setUp(scn.inject(rampUsers(250) over (60 seconds)).protocols(httpConf))
+	setUp(scn.inject(
+		rampUsers(100) over (60 seconds)
+	).protocols(httpConf))
 }
