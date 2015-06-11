fc7138e2290f32d5b7666316c852eb36e741576e
Ho Yan Leung <hy.leung@gmail.com>
2015-06-10 20:11:35 -0700

    Removes debug setting on startup

--- api-gateway/scripts/startup.sh
+++ api-gateway/scripts/startup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-echo "Starting $STORE_INVENTORY_HOST"
-echo "Starting $INVENTORY_HOST"
-
-java -Xdebug -Xrunjdwp:transport=dt_socket,address=9999,server=y,suspend=n -Dinventory.service.host=$INVENTORY_HOST -Davailability.service.host=$STORE_INVENTORY_HOST -jar api-gateway.jar
+java -Dinventory.service.host=$INVENTORY_HOST -Davailability.service.host=$STORE_INVENTORY_HOST -jar api-gateway.jar
