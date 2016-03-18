63e27fa5fbd8f86e68dbfabe26069158046379a2
Ho Yan Leung <hy.leung@gmail.com>
2016-03-18 06:59:19 -0700

    Adds Guice support

--- api-gateway/src/main/java/com/example/server/App.java
+++ api-gateway/src/main/java/com/example/server/App.java
@@ -1,12 +1,22 @@
 package com.example.server;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.servlet.ServletModule;
 import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import com.squarespace.jersey2.guice.JerseyGuiceModule;
+import com.squarespace.jersey2.guice.JerseyGuiceUtils;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,6 +27,21 @@ import org.slf4j.LoggerFactory;
 public class App {
 	private static Logger LOGGER = LoggerFactory.getLogger(App.class);
 	public static void main(String[] args) {
+        List<Module> modules = new ArrayList<>();
+
+        modules.add(new JerseyGuiceModule("__HK2_Generated_0"));
+        modules.add(new ServletModule());
+        modules.add(new AbstractModule() {
+            @Override
+            protected void configure() {
+                // ...
+            }
+        });
+
+        Injector injector = Guice.createInjector(modules);
+        JerseyGuiceUtils.install(injector);
+
+
 		final String contextPath = System.getProperty("jettyRun.contextPath","/");
 		final int port = Integer.parseInt(System.getProperty("jettyRun.httpPort", "8080"));
 		LOGGER.info("Starting Jetty on port {}, context path => {}", port, contextPath);
